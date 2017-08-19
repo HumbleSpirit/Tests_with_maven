b08240203d6ce4ab87d1de103ec6232d81afed87
sergey <sergey@sergey-HP>
2017-08-19 17:26:31 +0300

    Completed Test 2

--- src/test/java/NewEmptyJUnitTest.java
+++ src/test/java/NewEmptyJUnitTest.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,43 +9,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-/**
- *
- * @author sergey
- */
-public class NewEmptyJUnitTest {
+public class NewEmptyJUnitTest
+{
     
-    public NewEmptyJUnitTest() {
+    public NewEmptyJUnitTest() 
+    {
+        
     }
     
     private WebDriver driver;
     private WebDriver wait;
     
     @Before
-    public void setUp() {
+    public void setUp()
+    {
         driver = new ChromeDriver();
-    
     }
     
     @Test
-    public void MyFirstTest () throws InterruptedException {
+    public void MyFirstTest () throws InterruptedException 
+    {
         driver.get("http://bash.im");
         driver.findElement(By.id("text")).sendKeys("185");
         driver.findElement(By.xpath("//*[@id=\"search\"]/button")).click();
-        
     }
     
     @After
-    public void tearDown() {
-         driver.quit();
+    public void tearDown()
+    {
+        driver.quit();
         driver = null;
     }
     
     
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
